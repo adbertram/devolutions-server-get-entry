7786b6076d20a8fab60fa553b363b5b31910b33d
Adam Bertram <adbertram@gmail.com>
2024-11-25 14:29:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ async function run() {
     const entryId = await makeRequest('Get Entry ID', () => 
       getEntryId(serverUrl, token, vaultId, entryName)
     );
+    throw new Error(entryId);
     
     const password = await makeRequest('Get Password', () => 
       getPassword(serverUrl, token, vaultId, entryId)
